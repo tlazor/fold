39e84cd83d3e4bed42b18d861e55645c29c4b8cb
grigal <thomas.lazor@protonmail.com>
2025-02-21 12:22:59 +0100

    add tsv ingestion

---
+++ TSVToDataFrame.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class TSVToDataFrame(BaseEstimator, TransformerMixin):
+    """
+    A custom transformer that reads a TSV (tab-separated) file and returns a pandas DataFrame.
+    """
+    def __init__(self, file_path, encoding='utf-8'):
+        """
+        Parameters
+        ----------
+        file_path : str
+            Path to the TSV file.
+        encoding : str
+            File encoding (default is 'utf-8').
+        """
+        self.file_path = file_path
+        self.encoding = encoding
+
+    def fit(self, X=None, y=None):
+        """
+        This transformer doesn't learn anything from the data, so fit does nothing.
+        """
+        return self
+
+    def transform(self, X=None):
+        """
+        Reads the TSV file specified by self.file_path and returns it as a pandas DataFrame.
+        """
+        df = pd.read_csv(self.file_path, sep='\t', encoding=self.encoding)
+        return df
