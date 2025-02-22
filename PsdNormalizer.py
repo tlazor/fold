7d76a88fe5e0ffcb8f20d2ad03e4fc8054ae1421
grigal <thomas.lazor@protonmail.com>
2025-02-22 09:05:15 +0100

    add psd normalization

---
+++ PsdNormalizer.py
@@ -0,0 +1,16 @@
+import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
+from scipy.signal import welch
+
+
+class PsdNormalizer(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+
+    def fit(self, X, y=None):
+        return self
+    
+
+    def transform(self, X):
+        return [x / np.sum(x, axis=-1, keepdims=True) for x in X]
