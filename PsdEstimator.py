877c6a4531807ad7b38baf8985cb8c162fbdfc20
grigal <thomas.lazor@protonmail.com>
2025-02-22 09:04:50 +0100

    add psd estimation using welch

---
+++ PsdEstimator.py
@@ -0,0 +1,17 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+from scipy.signal import welch
+
+
+class PsdEstimator(BaseEstimator, TransformerMixin):
+    def __init__(self, nperseg=10, window='hann', scaling='density'):
+        self.nperseg = nperseg
+        self.window = window
+        self.scaling = scaling
+
+    def fit(self, X, y=None):
+        return self
+    
+
+    def transform(self, X):
+        # f, Pxx = welch
+        return [welch(x, fs=1, nperseg=self.nperseg, window=self.window, scaling=self.scaling)[1] for x in X]
