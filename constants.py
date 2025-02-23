262e1c450cf0cff7491e3598092901f40134465a
grigal <thomas.lazor@protonmail.com>
2025-02-23 12:56:57 +0100

    add fsi scale for comparison

--- constants.py
+++ constants.py
@@ -1,1 +1,68 @@
+# languages in XNLI dataset
 LANGUAGES = ["ar", "bg", "de", "el", "en", "es", "fr", "hi", "ru", "sw", "th", "tr", "ur", "vi", "zh"]
+
+# weeks to reach an integrated score of 3 (Speaking + Listening) on the Interagency Language Roundtable (ILR) scale
+FSI_SCALE = {
+    # Category I
+    "DA": 24,  # Danish
+    "NL": 24,  # Dutch
+    "FR": 30,  # French
+    "IT": 24,  # Italian
+    "NO": 24,  # Norwegian
+    "PT": 24,  # Portuguese
+    "RO": 24,  # Romanian
+    "ES": 30,  # Spanish
+    "SV": 24,  # Swedish
+    # Category II
+    "DE": 36,  # German
+    "HT": 36,  # Haitian Creole
+    "ID": 36,  # Indonesian
+    "MS": 36,  # Malay
+    "SW": 36,  # Swahili
+    # Category III
+    "SQ": 44,  # Albanian
+    "AM": 44,  # Amharic
+    "HY": 44,  # Armenian
+    "AZ": 44,  # Azerbaijani
+    "BN": 44,  # Bengali
+    "BG": 44,  # Bulgarian
+    "MY": 44,  # Burmese
+    "CS": 44,  # Czech
+    "FA": 44,  # Dari (Farsi)
+    "ET": 44,  # Estonian
+    "FI": 44,  # Finnish
+    "KA": 44,  # Georgian
+    "EL": 44,  # Greek
+    "HE": 44,  # Hebrew
+    "HI": 44,  # Hindi
+    "HU": 44,  # Hungarian
+    "KK": 44,  # Kazakh
+    "KM": 44,  # Khmer
+    "KU": 44,  # Kurdish
+    "KY": 44,  # Kyrgyz
+    "LO": 44,  # Lao
+    "LV": 44,  # Latvian
+    "LT": 44,  # Lithuanian
+    "MK": 44,  # Macedonian
+    "MN": 44,  # Mongolian
+    "NE": 44,  # Nepali
+    "PL": 44,  # Polish
+    "RU": 44,  # Russian
+    "SR": 44,  # Serbo-Croatian
+    "SK": 44,  # Slovak
+    "SL": 44,  # Slovenian
+    "TG": 44,  # Tajiki
+    "TH": 44,  # Thai
+    "TR": 44,  # Turkish
+    "TK": 44,  # Turkmen
+    "UK": 44,  # Ukrainian
+    "UR": 44,  # Urdu
+    "UZ": 44,  # Uzbek
+    "VI": 44,  # Vietnamese
+    # Category IV
+    "AR": 88,  # Arabic
+    "YUE": 88,  # Chinese - Cantonese
+    "ZH": 88,  # Chinese - Mandarin
+    "JA": 88,  # Japanese
+    "KO": 88   # Korean
+}
