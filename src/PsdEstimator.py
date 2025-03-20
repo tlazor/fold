da5af9197b2fb039df854c0723dcb64b8013cb88
grigal <thomas.lazor@protonmail.com>
2025-03-20 07:33:34 +0000

    adjust psd for 3d case

--- src/PsdEstimator.py
+++ src/PsdEstimator.py
@@ -3,20 +3,22 @@ from scipy.signal import welch
 
 
 class PsdEstimator(BaseEstimator, TransformerMixin):
-    def __init__(self, nperseg=10, window="hann", scaling="density"):
+    def __init__(self, nperseg=10, window="hann", scaling="density", axis=-1):
         self.nperseg = nperseg
         self.window = window
         self.scaling = scaling
+        self.axis = axis
 
     def fit(self, X, y=None):
         return self
 
     def transform(self, X):
-        # f, Pxx = welch
-        # return [welch(x, average='median')[1] for x in X]
+        # shape: [num_langs, num_tokens, hidden_dim]
+        print(f"{X[0].shape=}")
+
         return [
             welch(
-                x, fs=1, nperseg=self.nperseg, window=self.window, scaling=self.scaling
+                x, fs=1, nperseg=self.nperseg, window=self.window, scaling=self.scaling, axis=self.axis
             )[1]
             for x in X
         ]
--- src/PsdNormalizer.py
+++ src/PsdNormalizer.py
@@ -4,11 +4,13 @@ from scipy.signal import welch
 
 
 class PsdNormalizer(BaseEstimator, TransformerMixin):
-    def __init__(self):
-        pass
+    def __init__(self, axis=-1):
+        self.axis = axis
 
     def fit(self, X, y=None):
         return self
 
     def transform(self, X):
-        return [x / np.sum(x, axis=-1, keepdims=True) for x in X]
+        print(f"{X[0].shape=}")
+        eps=1e-9
+        return [x / (np.sum(x, axis=self.axis, keepdims=True)+eps) for x in X]
